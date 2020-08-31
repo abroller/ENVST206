9541fd5535348c781e941aeaf73c83677b1aefed
Abby Roller <aroller@hamilton.edu>
2020-08-31 16:28:15 -0400

    practicing mathematic operations

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,18 @@
 # Activity 1
 print("hello R world")
+
+#trying different calculator functions
+#6 raised to the 6th
+6^6
+#5 plus 210
+5+210
+
+#name my number
+x <- 500
+#multiply 500 by 10
+x*10
+
+#make a vector
+y <- c(300,200,100,50)
+#divide all numbers by 2
+y/2
