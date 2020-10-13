592aee837312dda68476c3af2a256ca5d74209c1
Abby Roller <aroller@hamilton.edu>
2020-10-13 19:22:11 -0400

    match glacier names

--- activity6/activity6_script.R
+++ activity6/activity6_script.R
@@ -13,7 +13,31 @@ library(dplyr)
   #readOGR in rgdal reads in a shape file 
   #will tell R to look for all the related files
 #glaciers in 1966
-g1966 <- readOGR("/Users/abby/Documents/ENVST206/ao6/GNPglaciers/GNPglaciers_1966.shp")
-
+g1966 <- readOGR("/Users/abby/Documents/ENVST206/a06/GNPglaciers/GNPglaciers_1966.shp")
 #glaciers in 2015
-g2015 <- readOGR("/Users/abby/Documents/ENVST206/ao6/GNPglaciers/GNPglaciers_2015.shp")
+g2015 <- readOGR("/Users/abby/Documents/ENVST206/a06/GNPglaciers/GNPglaciers_2015.shp")
+
+#investigate format of data
+str(g2015)
+
+#map the glaciers with specified colors
+plot(g1966, col = "lightblue2", border="grey50")
+
+#preview the first 6 lines and columns of shape file data table 
+  #using @ is a feature specific to spatial data
+head(g2015@data)
+
+#vector object projection info
+g1966@proj4string
+
+#check glacier names (they don't match exactly)
+g1966@data$GLACNAME
+g2015@data$GLACNAME
+#fix glacier names to be consistent across years
+g2015@data$GLACNAME <-ifelse(g2015@data$GLACNAME == "North Swiftcurrent Glacier",
+                             "N. Swiftcurrent Glacier",
+                             ifelse(g2015@data$GLACNAME == "Miche Wabun",
+                                    "Miche Wabun Glacier",
+                                    as.character(g2015@data$GLACNAME)))
+
+
