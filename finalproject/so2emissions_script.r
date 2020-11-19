271db7bc54a2e5404dacfb26802fb08d3e7f40cd
Abby Roller <aroller@hamilton.edu>
2020-11-19 11:49:53 -0500

    finish visualizations

--- finalproject/so2emissions_script.r
+++ finalproject/so2emissions_script.r
@@ -82,9 +82,13 @@ SO2stats$avg.DE <- aggregate(SO2.DE$SO2, by=list(SO2.DE$Year), FUN="mean")$x
 SO2stats$sd.DE <- aggregate(SO2.DE$SO2, by=list(SO2.DE$Year), FUN="sd")$x
 colnames(SO2stats) <- c("Year","NY Avg", "NY StdDev", "OH Avg", "OH StdDev", "DE Avg", "DE StdDev")
 
-## figure out how to calculate emissions reductions as bottom row
-
-
+## calculate emissions reductions
+# in NY
+SO2stats[1,2]-SO2stats[2,2]
+# in OH
+SO2stats[1,4]-SO2stats[2,4]
+# in DE
+SO2stats[1,6]-SO2stats[2,6]
 
 
 ### t-test: is there a reduction between years ? ----
@@ -116,7 +120,7 @@ bartlett.test(SO2.OH$SO2 ~ as.factor(SO2.OH$Year))
 bartlett.test(SO2.DE$SO2 ~ as.factor(SO2.DE$Year))
 ## not equal variance 
 
-# test significance of changes in SO2 --- none of the cities meet assumptions
+# test significance of changes in SO2 --- not valid bc assumptions not met
 t.test(SO2.NY$SO2[SO2.NY$Year == "1990"],SO2.NY$SO2[SO2.NY$Year == "2008"], alternative = "greater")
 t.test(SO2.OH$SO2[SO2.OH$Year == "1990"],SO2.OH$SO2[SO2.OH$Year == "2008"], alternative = "greater")
 t.test(SO2.DE$SO2[SO2.DE$Year == "1990"],SO2.DE$SO2[SO2.DE$Year == "2008"], alternative = "greater")
@@ -133,50 +137,74 @@ bartlett.test(SO2.all$SO2[SO2.all$Year == "2008"] ~ SO2.all$cityID[SO2.all$Year 
 
 ## Visualizations ----
 
-# make box plots to compare distribution by year for each site
-ggplot(data = SO2.NY, aes(x=Year, y=SO2))+
-  geom_violin(fill="lightsteelblue1")+
-  geom_boxplot(width=0.1, size=0.25, alpha=.5)+
-  theme_classic()+
-  labs(y= "Daily Max 1-hour SO2 Concentration", title="Distribution of SO2 Concentration in NYC area")
-
-ggplot(data = SO2.OH, aes(x=Year, y=SO2, group=Year))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(y= "Daily Max 1-hour SO2 Concentration", title="Distribution of SO2 Concentration in Cleveland, OH area")
-
-ggplot(data = SO2.DE, aes(x=Year, y=SO2, group=Year))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(y= "Daily Max 1-hour SO2 Concentration", title="Distribution of SO2 Concentration in Delaware area")
-
-## add in violin plots
-
-
-# make box plots to visually analyze instead
-ggplot(data=SO2.1990, aes(x=cityID, y=SO2))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(x= "City ID", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="SO2 Concentrations in 1990")
-
-ggplot(data=SO2.2008, aes(x=cityID, y=SO2))+
-  geom_boxplot()+
-  theme_classic()+
-  labs(x= "City ID", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="SO2 Concentrations in 2008")
-
 # boxplot of all cities and both years
 ggplot(data=SO2.all, aes(x=cityID, y=SO2, fill=Year))+
-  geom_boxplot()+
+  geom_boxplot(outlier.size=.5)+
+  scale_fill_manual(values=c("salmon","royalblue1"))+
   theme_classic()+
-  labs(x= "City ID", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="Distributions of Sulfur Dioxide Concentrations")
+  labs(x= "Urban Area", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="Annual Distributions of Sulfur Dioxide Concentrations")+
+  theme(plot.title = element_text(size = 20,hjust=0.5))
+ 
 
+# make separate box plots of each urban area to compare distribution by year
+ggplot(data = SO2.NY, aes(x=Year, y=SO2, fill=Year))+
+  geom_violin(fill="gray88")+
+  geom_boxplot(width=0.1, size=0.25, outlier.size=.5)+
+  scale_fill_manual(values=c("salmon","royalblue1"))+
+  theme_classic()+
+  labs(y= "Daily Max 1-hour SO2 Concentration", title="Sulfur Dioxide Concentration in New York-Newark-Jersey City Area")+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
+
+ggplot(data = SO2.OH, aes(x=Year, y=SO2, fill=Year))+
+  geom_violin(fill="gray88")+
+  geom_boxplot(width=0.1, size=0.25, outlier.size=.5)+
+  scale_fill_manual(values=c("salmon","royalblue1"))+
+  theme_classic()+
+  labs(y= "Daily Max 1-hour SO2 Concentration", title="Sulfur Dioxide Concentration in Cleveland-Elyria Area")+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
+
+ggplot(data = SO2.DE, aes(x=Year, y=SO2, fill=Year))+
+  geom_violin(fill="gray88")+
+  geom_boxplot(width=0.1, size=0.25, outlier.size=.5)+
+  scale_fill_manual(values=c("salmon","royalblue1"))+
+  theme_classic()+
+  labs(y= "Daily Max 1-hour SO2 Concentration", title="Sulfur Dioxide Concentration in Philadelphia-Camden-Wilmington Area")+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
+
+
+# make separate box plots by year to compares distributions across locations
+ggplot(data=SO2.1990, aes(x=cityID, y=SO2))+
+  geom_boxplot(fill="mistyrose",outlier.size=.5)+
+  theme_classic()+
+  labs(x= "Urban Area", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="Sulfur Dioxide Concentrations in 1990")+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
+
+ggplot(data=SO2.2008, aes(x=cityID, y=SO2))+
+  geom_boxplot(fill="lightsteelblue1", outlier.size=.5)+
+  theme_classic()+
+  labs(x= "Urban Area", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="Sulfur Dioxide Concentrations in 2008")+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
 
 # line graph of one station in NY
 ggplot(data=NYstn, aes(x=DOY, y=SO2, color=Year))+
   geom_path()+
   labs(x="Day of Year", y="Daily Maximum 1-hour SO2 Concentration (ppb)", title="Sulfur Dioxide Concentration at NY Station 360590005")+
   theme_classic()+
-  scale_color_manual(values = c("salmon","royalblue"))
+  scale_color_manual(values = c("salmon","royalblue1"))+
+  theme(plot.title = element_text(size = 15, hjust=0.5))
+
+# try to run a t test for NY station
+# test for normality
+shapiro.test(NYstn$SO2[NYstn$Year == "1990"])
+shapiro.test(NYstn$SO2[NYstn$Year == "2008"])
+# fails normality test
+# test for equal variance
+bartlett.test(NYstn$SO2 ~ NYstn$Year)
+# fails equal variance test
+# so we can't run a t test for this station 
+
+mean(NYstn$SO2[NYstn$Year == "1990"])
+mean(NYstn$SO2[NYstn$Year == "2008"])
 
 ## to clean up:
 # make one plot comparing changes over time (all cities)
